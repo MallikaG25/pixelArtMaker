266584e04a2a9f80e818b9077db0a1624782a220
MallikaG25 <92316075+MallikaG25@users.noreply.github.com>
2022-02-05 14:49:09 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,34 @@
+// Select color input
+
+// Select size input
+let pixelTable = document.querySelector("#pixelCanvas");
+
+
+let sizePiker = document.getElementById("sizePiker");
+sizePicker.addEventListener('submit',function(event) {
+    event.preventDefault();
+    let row = document.querySelector("#inputHeight").value;
+    let column = document.querySelector("#inputWidth").value;
+    makeGrid(row,column);
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(row,column) {
+    pixelTable.innerHTML = "";
+    for(var i=0;i<=row-1;i++){
+        var rowInsertion = pixelTable.insertRow(i);
+        console.log("mallika");
+        for(var j=0;j<=column-1;j++){
+            var cellInsertion = rowInsertion.insertCell(j);
+            colourChange(cellInsertion);
+        };
+    };
+}
+
+function colourChange(cellInsertion){
+    var color = document.getElementById("colorPicker");
+    cellInsertion.addEventListener("click",function(event) {
+        event.target.style.backgroundColor = color.value;
+    });
+}
